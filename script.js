607114ee75edd748c0fcd8ade7ca174b3c5dd829
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2025-05-24 00:32:57 +0530

    feat: :sparkles: Add Funtionality To The Calculator

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let firstOperand = "";
 let secondOperand = "";
 let operator = "";
 let mainScreenContent = "";
+let miniScreenContent = "";
 // Functions
 const add = (num1, num2) => {
   return num1 + num2;
@@ -23,39 +24,109 @@ const divide = (num1, num2) => {
 };
 const operate = function (op1, op2, operator) {
   if (operator == "+") {
-    return add(op1, op2);
+    return add(+op1, +op2);
   } else if (operator == "-") {
-    return subtract(op1, op2);
+    return subtract(+op1, +op2);
   } else if (operator == "*") {
-    return multiply(op1, op2);
+    return multiply(+op1, +op2);
   } else {
-    return divide(op1, op2);
+    return divide(+op1, +op2);
   }
 };
-const setMainOutput = function (out) {
-  mainScreen.innerText = out;
+const clearScreenContent = function () {
+  mainScreenContent = "";
+  miniScreenContent = "";
+  mainScreen.innerHTML = "";
+  miniScreen.innerHTML = "";
 };
+const clearMainScreenContent = function () {
+  mainScreenContent = "";
+  mainScreen.innerHTML = "";
+};
+const clearMiniScreenContent = function () {
+  miniScreenContent = "";
+  miniScreen.innerHTML - "";
+};
+const setScreenContent = function (content, screen) {
+  screen.innerHTML = content;
+};
+const clearOperationValues = function () {
+  firstOperand = "";
+  secondOperand = "";
+  operator = "";
+};
+const checkOperationValues = () => firstOperand && secondOperand && operator;
+const removeLastLetter = function (str) {
+  return str.slice(0, str.length - 1);
+};
+const handleInput = function (inputValue) {
+  const operatorsList = "+-*/";
+  if (inputValue === "ac") {
+    clearScreenContent();
+    clearOperationValues();
+  } else if (inputValue == "c") {
+    mainScreenContent = removeLastLetter(mainScreenContent);
+    if (secondOperand) {
+      secondOperand = removeLastLetter(secondOperand);
+    } else if (firstOperand) {
+      firstOperand = removeLastLetter(firstOperand);
+    }
+    miniScreenContent = mainScreenContent;
+    setScreenContent(mainScreenContent, mainScreen);
+  } else if (operatorsList.includes(inputValue)) {
+    if (checkOperationValues()) {
+      miniScreenContent = operate(firstOperand, secondOperand, operator);
+      clearOperationValues();
+      firstOperand = miniScreenContent;
+      operator = inputValue;
+      miniScreenContent += inputValue;
+      setScreenContent(miniScreenContent, miniScreen);
+      clearMainScreenContent();
+    } else if (!secondOperand && operator) {
+    } else {
+      operator = inputValue;
+      miniScreenContent += inputValue;
+      setScreenContent(miniScreenContent, miniScreen);
+      clearMainScreenContent();
+    }
+  } else if (inputValue === "=") {
+    if (checkOperationValues()) {
+      setScreenContent(miniScreenContent, miniScreen);
+      mainScreenContent = operate(firstOperand, secondOperand, operator);
+      //   firstOperand = mainScreenContent;
+      setScreenContent(mainScreenContent, mainScreen);
+    }
+  } else if (inputValue === ".") {
+    if (firstOperand) {
+      if (firstOperand && operator) {
+        secondOperand += inputValue;
+      } else {
+        firstOperand += inputValue;
+      }
+      mainScreenContent += inputValue;
+      miniScreenContent += inputValue;
+      setScreenContent(mainScreenContent, mainScreen);
+    }
+  } else {
+    if (firstOperand && operator) {
+      secondOperand += inputValue;
+    } else {
+      firstOperand += inputValue;
+    }
+    mainScreenContent += inputValue;
+    miniScreenContent += inputValue;
+    setScreenContent(mainScreenContent, mainScreen);
+  }
+};
+
+// Add Event Listener For Each Btn
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    targetId = e.target.id;
-    let operatorsList = "+-*/";
-    if (targetId == "=") {
-      console.log("equals");
-    } else if (operatorsList.includes(targetId)) {
-      if (mainScreenContent !== "") {
-        operator = targetId;
-        mainScreenContent += operator;
-        setMainOutput(mainScreenContent);
-      }
-    } else {
-      firstOperand += targetId;
-      mainScreenContent = firstOperand;
-      setMainOutput(mainScreenContent);
-    }
+    handleInput(e.target.id);
   });
 });
-// Setting Color Mode
 
+// Setting Color Mode
 const setCurrentColorTheme = function (theme) {
   if (theme == "light") {
     rootElement.classList.add("light");
@@ -68,6 +139,6 @@ colorButtons.forEach((btn) => {
     setCurrentColorTheme(e.target.id);
     const activeThemeElement = document.querySelector(".color-mode .fa-beat");
     activeThemeElement.classList.remove("fa-beat");
-    console.log(e.target.classList.add("fa-beat"));
+    e.target.classList.add("fa-beat")
   });
 });
